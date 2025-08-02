f065ae7cf394cd63533d145bbc56ff16938f1f4a
shubham143189 <kumarshubhams@outlook.com>
2025-08-02 14:42:40 +0530

    Create button.jsx

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,7 @@
+export function Button({ children, className, ...props }) {
+  return (
+    <button className={`bg-blue-500 text-white px-4 py-2 rounded ${className}`} {...props}>
+      {children}
+    </button>
+  );
+}
